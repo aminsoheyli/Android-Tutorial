a394beb64cf8aa27b967d85182f44789bd3423a0
Amin Soheyli <aminsoheyli98@gmail.com>
2022-06-19 14:59:44 +0430

    Add alert dialog

--- app/src/main/java/com/aminsoheyli/androidtutorial/MainActivity.kt
+++ app/src/main/java/com/aminsoheyli/androidtutorial/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.aminsoheyli.androidtutorial
 
+import android.content.DialogInterface
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
-import androidx.fragment.app.FragmentManager
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var buttonDialog: Button
+    private lateinit var buttonAlert: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,19 +19,37 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun initUi() {
-        buttonDialog = findViewById(R.id.button_show)
+        buttonDialog = findViewById(R.id.button_show_dialog)
+        buttonAlert = findViewById(R.id.buton_show_alert)
+
         buttonDialog.setOnClickListener {
             val popTime = PopTime()
             popTime.show(supportFragmentManager, "Show dialog fragment")
         }
+
+        buttonAlert.setOnClickListener {
+            val alert = AlertDialog.Builder(this)
+            alert.setMessage("Delete document?")
+                .setTitle("Confirmation")
+                .setPositiveButton("Yes") { dialog, which ->
+                    showSnackBar("✅ Deleted")
+                }
+                .setNegativeButton("No") { dialog, which ->
+                    showSnackBar("❎ Canceled")
+                }
+            alert.show()
+        }
+
     }
 
     fun setTime(time: String) {
-        Snackbar.make(
-            buttonDialog,
-            "Time: $time",
-            Snackbar.LENGTH_LONG
-        ).setTextColor(Color.YELLOW)
+        val text = "Time: $time";
+        showSnackBar(text)
+    }
+
+    private fun showSnackBar(text: String) {
+        Snackbar.make(buttonDialog, text, Snackbar.LENGTH_LONG)
+            .setTextColor(Color.YELLOW)
             .show()
     }
 
