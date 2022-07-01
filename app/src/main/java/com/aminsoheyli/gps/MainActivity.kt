8e0f5cd879a47f39c1b3163e0bde37c9f1e550a2
Amin Soheyli <aminsoheyli98@gmail.com>
2022-07-01 14:50:56 +0430

    Add current live location feature

--- app/src/main/java/com/aminsoheyli/gps/MainActivity.kt
+++ app/src/main/java/com/aminsoheyli/gps/MainActivity.kt
@@ -2,9 +2,11 @@ package com.aminsoheyli.gps
 
 import android.Manifest.permission.ACCESS_COARSE_LOCATION
 import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.location.Location
 import android.location.LocationManager
+import android.location.LocationManager.GPS_PROVIDER
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -14,7 +16,8 @@ import androidx.core.content.ContextCompat
 
 const val REQUEST_CODE_ASK_LOCATION_PERMISSION = 1
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MyLocationUpdateInterface {
+    private var isLocationShown = false
     private lateinit var textViewLocation: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,20 +28,39 @@ class MainActivity : AppCompatActivity() {
             showLocation()
         }
 
+        if (isLocationPermissionGranted()) {
 
+        }
     }
 
+    @SuppressLint("MissingPermission")
     private fun showLocation() {
-        val location = getLastKnownLocation()
-        if (location != null)
-            textViewLocation.text = String.format(
-                resources.getString(R.string.textView_show_location_text),
-                location.longitude,
-                location.latitude
-            )
+        if (isLocationPermissionGranted()) {
+            if (!isLocationShown) {
+                // Show last known location
+                val location = getLastKnownLocation()
+                if (location != null)
+                    textViewLocation.text = String.format(
+                        resources.getString(R.string.textView_show_location_text),
+                        location.longitude,
+                        location.latitude
+                    )
+                isLocationShown = true
+            }
+            // Show current location with minTimeMs=3000 and minDistanceM=10
+            val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+            val locationListener = MyLocationListener(this, this)
+            locationManager.requestLocationUpdates(GPS_PROVIDER, 3000L, 10f, locationListener)
+        }
     }
 
+    @SuppressLint("MissingPermission")
     private fun getLastKnownLocation(): Location? {
+        val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+        return locationManager.getLastKnownLocation(GPS_PROVIDER)
+    }
+
+    private fun isLocationPermissionGranted(): Boolean {
         if (ContextCompat.checkSelfPermission(
                 this,
                 ACCESS_FINE_LOCATION
@@ -47,16 +69,15 @@ class MainActivity : AppCompatActivity() {
                 this,
                 ACCESS_COARSE_LOCATION
             ) == PERMISSION_GRANTED
-        ) {
-            val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
-            return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-        } else if (!ActivityCompat.shouldShowRequestPermissionRationale(this, ACCESS_FINE_LOCATION))
+        )
+            return true
+        else if (!ActivityCompat.shouldShowRequestPermissionRationale(this, ACCESS_FINE_LOCATION))
             ActivityCompat.requestPermissions(
                 this,
                 arrayOf(ACCESS_FINE_LOCATION),
                 REQUEST_CODE_ASK_LOCATION_PERMISSION
             )
-        return null
+        return false
     }
 
     override fun onRequestPermissionsResult(
@@ -73,4 +94,14 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    override fun onLocationUpdate(location: Location) {
+        textViewLocation.text = String.format(
+            resources.getString(R.string.textView_show_location_text),
+            location.longitude,
+            location.latitude
+        )
+    }
+
 }
+
---
+++ app/src/main/java/com/aminsoheyli/gps/MyLocationListener.kt
@@ -0,0 +1,34 @@
+package com.aminsoheyli.gps
+
+import android.content.Context
+import android.location.Location
+import android.location.LocationListener
+import android.os.Bundle
+import android.widget.Toast
+
+class MyLocationListener(
+    private val context: Context,
+    private val locationUpdateInterface: MyLocationUpdateInterface
+) : LocationListener {
+    override fun onLocationChanged(location: Location) {
+        val message = String.format(
+            context.resources.getString(R.string.textView_show_location_text),
+            location.longitude,
+            location.latitude
+        )
+        locationUpdateInterface.onLocationUpdate(location)
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+        Toast.makeText(context, "GPS status changed to $status", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onProviderEnabled(provider: String) {
+        Toast.makeText(context, "GPS is enabled", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onProviderDisabled(provider: String) {
+        Toast.makeText(context, "GPS is disabled", Toast.LENGTH_LONG).show()
+    }
+}
