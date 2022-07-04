a3865cbd3407e99457ac4eaa9162ae3d9e8655d0
Amin Soheyli <aminsoheyli98@gmail.com>
2022-07-04 18:14:43 +0430

    Add banner ad by google AdMob

--- firebase/app/src/main/java/com/aminsoheyli/firebase/MainActivity.kt
+++ firebase/app/src/main/java/com/aminsoheyli/firebase/MainActivity.kt
@@ -5,6 +5,9 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -13,12 +16,34 @@ import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    lateinit var editText: EditText
+    lateinit var button: Button
+    lateinit var mAdView : AdView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val editText = findViewById<EditText>(R.id.editText)
-        val button = findViewById<Button>(R.id.button)
+        editText = findViewById(R.id.editText)
+        button = findViewById(R.id.button)
+
+        // Realtime Database
+        firebaseRealTimeDatabase()
+
+        // Banner Ad by AdMob
+        firebaseBannerAd()
+
+    }
+
+    private fun firebaseBannerAd() {
+        MobileAds.initialize(this) {}
+
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+    }
+
+    private fun firebaseRealTimeDatabase() {
         val database = Firebase.database(getString(R.string.firebase_real_time_database_url))
         val myRef = database.getReference("msg")
 
