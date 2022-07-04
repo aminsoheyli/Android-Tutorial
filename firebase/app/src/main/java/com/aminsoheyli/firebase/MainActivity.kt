6b40ccee336ae1cbb20f878006128afd2fc30826
Amin Soheyli <aminsoheyli98@gmail.com>
2022-07-04 17:59:33 +0430

    Add firebase config file and use firebase realtime database

--- firebase/app/src/main/java/com/aminsoheyli/firebase/MainActivity.kt
+++ firebase/app/src/main/java/com/aminsoheyli/firebase/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.aminsoheyli.firebase
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editText = findViewById<EditText>(R.id.editText)
+        val button = findViewById<Button>(R.id.button)
+        val database = Firebase.database(getString(R.string.firebase_real_time_database_url))
+        val myRef = database.getReference("msg")
+
+        button.setOnClickListener {
+            myRef.setValue(editText.text.toString())
+        }
+
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val value = dataSnapshot.getValue<String>()
+                editText.setText(value)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@MainActivity, "Failed to read value.", Toast.LENGTH_LONG).show()
+            }
+        })
     }
 }
