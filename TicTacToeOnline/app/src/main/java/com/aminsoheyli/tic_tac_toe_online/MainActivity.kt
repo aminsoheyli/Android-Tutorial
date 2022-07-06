8fe16165ff9b9ae6319cb66a3332fc5da5368f25
Amin Soheyli <aminsoheyli98@gmail.com>
2022-07-06 18:17:40 +0430

    Add tic-tac-toe game layout

--- TicTacToeOnline/app/src/main/java/com/aminsoheyli/tic_tac_toe_online/MainActivity.kt
+++ TicTacToeOnline/app/src/main/java/com/aminsoheyli/tic_tac_toe_online/MainActivity.kt
@@ -2,10 +2,15 @@ package com.aminsoheyli.tic_tac_toe_online
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onClick(v: View) {
+        TODO("Not yet implemented")
     }
 }
