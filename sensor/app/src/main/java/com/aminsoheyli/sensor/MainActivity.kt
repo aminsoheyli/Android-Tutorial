d661667d7c47b4ac026703ef905d51020c80b31e
Amin Soheyli <aminsoheyli98@gmail.com>
2022-07-11 01:53:32 +0430

    Add main activity layout

--- sensor/app/src/main/java/com/aminsoheyli/sensor/MainActivity.kt
+++ sensor/app/src/main/java/com/aminsoheyli/sensor/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.aminsoheyli.sensor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var buttonTakePicture: Button
+    private lateinit var imageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initUi()
+    }
+
+    private fun initUi() {
+        buttonTakePicture = findViewById(R.id.button_take_picture)
+        imageView = findViewById(R.id.imageView)
+        buttonTakePicture.setOnClickListener {
+        }
     }
 }
