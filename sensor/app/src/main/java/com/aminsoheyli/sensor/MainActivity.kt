5f842e44c32ba16512c3d3767c4ff44cf67b8941
Amin Soheyli <aminsoheyli98@gmail.com>
2022-07-11 18:50:00 +0430

    Use light sensor to play audio

--- sensor/app/src/main/java/com/aminsoheyli/sensor/MainActivity.kt
+++ sensor/app/src/main/java/com/aminsoheyli/sensor/MainActivity.kt
@@ -1,28 +1,46 @@
 package com.aminsoheyli.sensor
 
 import android.content.ActivityNotFoundException
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
 import androidx.core.app.ActivityCompat
+import java.io.IOException
 
 
 const val REQ_PERMISSION_WRITE_EXTERNAL_STORAGE = 1
 const val REQUEST_IMAGE_CAPTURE = 2
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var buttonTakePicture: Button
     private lateinit var imageView: ImageView
+    private lateinit var sensor: Sensor
+    private lateinit var sensorManager: SensorManager
+    private lateinit var mediaPlayer: MediaPlayer
+    private var isRunning = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initUi()
+        initSensors()
+    }
+
+    private fun initSensors() {
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+
     }
 
     private fun initUi() {
@@ -31,6 +49,7 @@ class MainActivity : AppCompatActivity() {
         buttonTakePicture.setOnClickListener {
             takePicture()
         }
+        mediaPlayer = MediaPlayer()
     }
 
     private fun takePicture() {
@@ -42,7 +61,8 @@ class MainActivity : AppCompatActivity() {
             val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             try {
                 startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-            } catch (e: ActivityNotFoundException) { }
+            } catch (e: ActivityNotFoundException) {
+            }
         } else
             ActivityCompat.requestPermissions(
                 this,
@@ -70,5 +90,36 @@ class MainActivity : AppCompatActivity() {
         when (requestCode) {
             REQ_PERMISSION_WRITE_EXTERNAL_STORAGE -> if (grantResults[0] == PackageManager.PERMISSION_GRANTED) takePicture()
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event!!.values[0] > 40)
+            if (!isRunning)
+                try {
+                    mediaPlayer.setDataSource("https://cdn6.iribtv.ir/9/original/2018/07/16/636673403410852046.mp3")
+                    mediaPlayer.prepare()
+                    mediaPlayer.start()
+                    isRunning = true
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            else
+                mediaPlayer.start()
+        else {
+            mediaPlayer.pause()
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
     }
 }
