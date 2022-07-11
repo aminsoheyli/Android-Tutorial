17d70b425602fdb79296660a83cc180cdbd8c6e5
Amin Soheyli <aminsoheyli98@gmail.com>
2022-07-11 17:51:43 +0430

    Add capturing picture and show thumbnail of the captured picture

--- sensor/app/src/main/java/com/aminsoheyli/sensor/MainActivity.kt
+++ sensor/app/src/main/java/com/aminsoheyli/sensor/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.aminsoheyli.sensor
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
+import androidx.core.app.ActivityCompat
+
+
+const val REQ_PERMISSION_WRITE_EXTERNAL_STORAGE = 1
+const val REQUEST_IMAGE_CAPTURE = 2
 
 class MainActivity : AppCompatActivity() {
     private lateinit var buttonTakePicture: Button
@@ -21,6 +29,46 @@ class MainActivity : AppCompatActivity() {
         buttonTakePicture = findViewById(R.id.button_take_picture)
         imageView = findViewById(R.id.imageView)
         buttonTakePicture.setOnClickListener {
+            takePicture()
+        }
+    }
+
+    private fun takePicture() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            try {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            } catch (e: ActivityNotFoundException) { }
+        } else
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                REQ_PERMISSION_WRITE_EXTERNAL_STORAGE
+            )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            REQUEST_IMAGE_CAPTURE -> if (resultCode == RESULT_OK) {
+                val imageBitmap = data?.extras?.get("data") as Bitmap
+                imageView.setImageBitmap(imageBitmap)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQ_PERMISSION_WRITE_EXTERNAL_STORAGE -> if (grantResults[0] == PackageManager.PERMISSION_GRANTED) takePicture()
         }
     }
 }
