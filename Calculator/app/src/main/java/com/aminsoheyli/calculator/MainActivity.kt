671e19c1cf29c2dfa6544d99ef060ab1fa451c54
Amin Soheyli <aminsoheyli98@gmail.com>
2022-07-14 16:41:15 +0430

    Add main activity layout

--- Calculator/app/src/main/java/com/aminsoheyli/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/aminsoheyli/calculator/MainActivity.kt
@@ -2,10 +2,19 @@ package com.aminsoheyli.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun onDigit(view: View) {}
+    fun onOperator(view: View) {}
+    fun onDecimalPoint(view: View) {}
+    fun onClear(view: View) {}
+    fun onEqual(view: View) {}
 }
