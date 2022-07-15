a8ac533d47dbb39f3cfe66a5e784e4f1a75fdc4d
Amin Soheyli <aminsoheyli98@gmail.com>
2022-07-15 20:14:45 +0430

    Add quiz qustion activity

--- QuizApp/app/src/main/java/com/aminsoheyli/quizapp/MainActivity.kt
+++ QuizApp/app/src/main/java/com/aminsoheyli/quizapp/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.aminsoheyli.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.WindowInsets
 import android.view.WindowInsetsController
+import android.widget.Toast
 import com.aminsoheyli.quizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +21,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initUi() {
+        binding.btnStart.setOnClickListener {
+            if (binding.etName.text.toString().isNotEmpty()) {
+                startActivity(Intent(this, QuizQustionActivity::class.java))
+                finish()
+            } else
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
 
+        }
     }
 }
---
+++ QuizApp/app/src/main/java/com/aminsoheyli/quizapp/QuizQustionActivity.kt
@@ -0,0 +1,11 @@
+package com.aminsoheyli.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizQustionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_qustion)
+    }
+}
