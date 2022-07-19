42e1f92c4651a3cec2e77e3d38f1c7ca0755f29f
Amin Soheyli <aminsoheyli98@gmail.com>
2022-07-19 23:56:29 +0430

    Add setBrushSize to the DrawingView to set size of brush outside of View

--- Paint/app/src/main/java/com/aminsoheyli/paint/DrawingView.kt
+++ Paint/app/src/main/java/com/aminsoheyli/paint/DrawingView.kt
@@ -3,17 +3,18 @@ package com.aminsoheyli.paint
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
 class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
-    private var drawPath: CustomPath? = null
-    private var drawPaint: Paint? = null
-    private var canvasBitmap: Bitmap? = null
-    private var canvasPaint: Paint? = null
+    private lateinit var drawPath: CustomPath
+    private lateinit var drawPaint: Paint
+    private lateinit var canvasBitmap: Bitmap
+    private lateinit var canvasPaint: Paint
+    private lateinit var canvas: Canvas
     private var brushSize: Float = 0f
     private var color: Int = Color.BLACK
-    private var canvas: Canvas? = null
     private var paths = ArrayList<CustomPath>()
 
     init {
@@ -23,39 +24,37 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
     private fun setupDrawing() {
         drawPath = CustomPath(color, brushSize)
         drawPaint = Paint()
-        drawPaint!!.apply {
+        drawPaint.apply {
             color = this@DrawingView.color
             style = Paint.Style.STROKE
             strokeJoin = Paint.Join.ROUND
             strokeCap = Paint.Cap.ROUND
         }
         canvasPaint = Paint(Paint.DITHER_FLAG)
-        brushSize = 20f
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
         canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
-        canvas = Canvas(canvasBitmap!!)
+        canvas = Canvas(canvasBitmap)
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        canvas.drawBitmap(canvasBitmap!!, 0f, 0f, canvasPaint)
+        canvas.drawBitmap(canvasBitmap, 0f, 0f, canvasPaint)
         for (path in paths)
             drawPath(canvas, path)
-        if (!drawPath!!.isEmpty)
-            drawPath(canvas, drawPath!!)
-        drawPaint!!.strokeWidth = drawPath!!.brushTickness
-        drawPaint!!.color = drawPaint!!.color
-        canvas.drawPath(drawPath!!, drawPaint!!)
-
+        if (!drawPath.isEmpty)
+            drawPath(canvas, drawPath)
+        drawPaint.strokeWidth = drawPath.brushTickness
+        drawPaint.color = drawPaint.color
+        canvas.drawPath(drawPath, drawPaint)
     }
 
     private fun drawPath(canvas: Canvas, path: CustomPath) {
-        drawPaint!!.strokeWidth = path.brushTickness
-        drawPaint!!.color = path.color
-        canvas.drawPath(path, drawPaint!!)
+        drawPaint.strokeWidth = path.brushTickness
+        drawPaint.color = path.color
+        canvas.drawPath(path, drawPaint)
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
@@ -63,22 +62,31 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         val y = event?.y
 
         when (event?.action) {
-            MotionEvent.ACTION_DOWN -> drawPath!!.apply {
+            MotionEvent.ACTION_DOWN -> drawPath.apply {
                 color = this@DrawingView.color
                 brushTickness = brushSize
                 reset()
                 if (x != null && y != null)
                     moveTo(x, y)
             }
-            MotionEvent.ACTION_MOVE -> if (x != null && y != null) drawPath!!.lineTo(x, y)
+            MotionEvent.ACTION_MOVE -> if (x != null && y != null) drawPath.lineTo(x, y)
             MotionEvent.ACTION_UP -> {
-                paths.add(drawPath!!)
+                paths.add(drawPath)
                 drawPath = CustomPath(color, brushSize)
             }
             else -> return false
         }
         invalidate()
         return true
+    }
+
+    fun setBrushSize(newSize: Float) {
+        brushSize = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            newSize,
+            resources.displayMetrics
+        )
+        drawPaint.strokeWidth = brushSize
     }
 
     internal inner class CustomPath(var color: Int, var brushTickness: Float) : Path()
--- Paint/app/src/main/java/com/aminsoheyli/paint/MainActivity.kt
+++ Paint/app/src/main/java/com/aminsoheyli/paint/MainActivity.kt
@@ -2,10 +2,20 @@ package com.aminsoheyli.paint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aminsoheyli.paint.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawingView: DrawingView
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUi()
+    }
+
+    private fun initUi() {
+        drawingView = binding.drawingView
+        drawingView.setBrushSize(20f)
     }
 }
