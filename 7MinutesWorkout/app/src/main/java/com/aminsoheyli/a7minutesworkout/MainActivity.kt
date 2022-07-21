3b36191a97cb5aebafe50d01f1a51275a37c4ea9
Amin Soheyli <aminsoheyli98@gmail.com>
2022-07-22 03:50:11 +0430

    Design main activity class and add ripple effect ot the start button

--- 7MinutesWorkout/app/src/main/java/com/aminsoheyli/a7minutesworkout/MainActivity.kt
+++ 7MinutesWorkout/app/src/main/java/com/aminsoheyli/a7minutesworkout/MainActivity.kt
@@ -2,10 +2,24 @@ package com.aminsoheyli.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+import com.aminsoheyli.a7minutesworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUi()
+    }
+
+    private fun initUi() {
+        val frameLayoutStart = binding.frameLayoutStart
+        frameLayoutStart.setOnClickListener {
+            Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
