53cf5afc341a03c481fcac909bf8b4c99d24eb90
Amin Soheyli <aminsoheyli98@gmail.com>
2022-07-22 18:50:24 +0430

    Add countDownTimer for progress bar to change

--- 7MinutesWorkout/app/src/main/java/com/aminsoheyli/a7minutesworkout/ExerciseActivity.kt
+++ 7MinutesWorkout/app/src/main/java/com/aminsoheyli/a7minutesworkout/ExerciseActivity.kt
@@ -2,11 +2,17 @@ package com.aminsoheyli.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
+import android.widget.Toast
 import com.aminsoheyli.a7minutesworkout.databinding.ActivityExerciseBinding
 import com.aminsoheyli.a7minutesworkout.databinding.ActivityMainBinding
 
 class ExerciseActivity : AppCompatActivity() {
     private lateinit var binding: ActivityExerciseBinding
+    private lateinit var restTimer: CountDownTimer
+    private var restProgress = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExerciseBinding.inflate(layoutInflater)
@@ -20,5 +26,28 @@ class ExerciseActivity : AppCompatActivity() {
         binding.toolbarExerciseActivity.setNavigationOnClickListener {
             onBackPressed()
         }
+        setRestProgressBar()
+    }
+
+    private fun setRestProgressBar() {
+        binding.progressBar.progress = restProgress
+        restTimer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                binding.progressBar.progress = 10 - restProgress
+                binding.textViewTimer.text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity, "Finish", Toast.LENGTH_SHORT).show()
+            }
+        }
+        restTimer.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        restTimer.cancel()
+        restProgress = 0
     }
 }
