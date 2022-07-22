621d02e8ec0cd81d74dd2d87f6a30f3c2e7aa3dd
Amin Soheyli <aminsoheyli98@gmail.com>
2022-07-22 19:55:56 +0430

    Add setup exercise and setup rest functions

--- 7MinutesWorkout/app/src/main/java/com/aminsoheyli/a7minutesworkout/ExerciseActivity.kt
+++ 7MinutesWorkout/app/src/main/java/com/aminsoheyli/a7minutesworkout/ExerciseActivity.kt
@@ -6,12 +6,11 @@ import android.os.CountDownTimer
 import android.util.Log
 import android.widget.Toast
 import com.aminsoheyli.a7minutesworkout.databinding.ActivityExerciseBinding
-import com.aminsoheyli.a7minutesworkout.databinding.ActivityMainBinding
 
 class ExerciseActivity : AppCompatActivity() {
     private lateinit var binding: ActivityExerciseBinding
     private lateinit var restTimer: CountDownTimer
-    private var restProgress = 0
+    private var progressValue = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,28 +25,39 @@ class ExerciseActivity : AppCompatActivity() {
         binding.toolbarExerciseActivity.setNavigationOnClickListener {
             onBackPressed()
         }
-        setRestProgressBar()
+        setupRest()
     }
 
-    private fun setRestProgressBar() {
-        binding.progressBar.progress = restProgress
-        restTimer = object : CountDownTimer(10000, 1000) {
+    private fun setupExercise() {
+        binding.textViewTitle.text = "Exercise Name"
+        setProgressBar(30000, 30) { setupRest() }
+    }
+
+    private fun setupRest() {
+        binding.textViewTitle.text = "GET READY FOR"
+        setProgressBar(10000, 10) { setupExercise() }
+    }
+
+    private fun setProgressBar(
+        duration: Long,
+        maxProgressValue: Int,
+        onFinishFunction: () -> Any
+    ) {
+        binding.progressBar.max = maxProgressValue
+        binding.progressBar.progress = progressValue
+        restTimer = object : CountDownTimer(duration, 1000) {
             override fun onTick(millisUntilFinished: Long) {
-                restProgress++
-                binding.progressBar.progress = 10 - restProgress
-                binding.textViewTimer.text = (10 - restProgress).toString()
+                progressValue++
+                val progress = maxProgressValue - progressValue
+                binding.progressBar.progress = progress
+                binding.textViewTimer.text = progress.toString()
             }
 
             override fun onFinish() {
-                Toast.makeText(this@ExerciseActivity, "Finish", Toast.LENGTH_SHORT).show()
+                restTimer.cancel()
+                progressValue = 0
+                onFinishFunction()
             }
-        }
-        restTimer.start()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        restTimer.cancel()
-        restProgress = 0
+        }.start()
     }
 }
