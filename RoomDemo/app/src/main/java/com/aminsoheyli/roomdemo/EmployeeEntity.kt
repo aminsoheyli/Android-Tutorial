00ec0a420ebd2d94f3156e45ed2e040fe2226085
Amin Soheyli <aminsoheyli98@gmail.com>
2022-07-27 13:18:12 +0430

    Add EmployeeEntity class

---
+++ RoomDemo/app/src/main/java/com/aminsoheyli/roomdemo/EmployeeEntity.kt
@@ -0,0 +1,15 @@
+package com.aminsoheyli.roomdemo
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "employee-table")
+data class EmployeeEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String = "",
+    // ColumnInfo -> name is the internal name for the column name of this table and it's optional
+    @ColumnInfo(name = "email-id")
+    val email: String = ""
+)
