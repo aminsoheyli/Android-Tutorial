29ec1f46de1c69130cb8599bdb5a37633ec572a3
Amin Soheyli <aminsoheyli98@gmail.com>
2022-08-04 15:25:37 +0430

    Add a feature to change profile image

--- TrelloClone/app/src/main/java/com/aminsoheyli/trelloclone/activities/MyProfileActivity.kt
+++ TrelloClone/app/src/main/java/com/aminsoheyli/trelloclone/activities/MyProfileActivity.kt
@@ -1,15 +1,25 @@
 package com.aminsoheyli.trelloclone.activities
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
-import android.view.WindowManager
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.aminsoheyli.trelloclone.R
 import com.aminsoheyli.trelloclone.databinding.ActivityMyProfileBinding
 import com.aminsoheyli.trelloclone.firebase.Firestore
 import com.aminsoheyli.trelloclone.models.User
+import com.aminsoheyli.trelloclone.utils.Constants
 import com.bumptech.glide.Glide
+import java.io.IOException
 
 class MyProfileActivity : BaseActivity() {
     private lateinit var binding: ActivityMyProfileBinding
+    private lateinit var selectedImageFileUri: Uri
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMyProfileBinding.inflate(layoutInflater)
@@ -24,13 +34,63 @@ class MyProfileActivity : BaseActivity() {
 
     private fun initUi() {
         setupActionBar()
+        binding.ivProfileUserImage.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+            )
+                Constants.showImageChooser(this@MyProfileActivity)
+            else
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    Constants.PERMISSION_CODE_READ_STORAGE
+                )
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK
+            && requestCode == Constants.REQUEST_CODE_PICK_IMAGE
+            && data?.data != null
+        ) {
+            selectedImageFileUri = data.data!!
+
+            try {
+                Glide
+                    .with(this@MyProfileActivity)
+                    .load(selectedImageFileUri)
+                    .centerCrop()
+                    .placeholder(R.drawable.ic_user_place_holder)
+                    .into(binding.ivProfileUserImage)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.PERMISSION_CODE_READ_STORAGE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                Constants.showImageChooser(this@MyProfileActivity)
+            else
+                Toast.makeText(
+                    this,
+                    "Oops, you just denied the permission for storage. You can also allow it from settings.",
+                    Toast.LENGTH_SHORT
+                ).show()
+        }
     }
 
     private fun setupActionBar() {
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
         val toolbar = binding.toolbarProfileActivity
         setSupportActionBar(toolbar)
         toolbar.setNavigationIcon(R.drawable.ic_action_navigation_menu)
@@ -47,7 +107,7 @@ class MyProfileActivity : BaseActivity() {
             .load(user.image)
             .centerCrop()
             .placeholder(R.drawable.ic_user_place_holder)
-            .into(binding.ivUserImage)
+            .into(binding.ivProfileUserImage)
 
         binding.etName.setText(user.name)
         binding.etEmail.setText(user.email)
--- TrelloClone/app/src/main/java/com/aminsoheyli/trelloclone/utils/Constants.kt
+++ TrelloClone/app/src/main/java/com/aminsoheyli/trelloclone/utils/Constants.kt
@@ -1,5 +1,20 @@
 package com.aminsoheyli.trelloclone.utils
 
+import android.app.Activity
+import android.content.Intent
+import android.provider.MediaStore
+
 object Constants {
     const val USERS = "users"
+
+    const val PERMISSION_CODE_READ_STORAGE = 1
+    const val REQUEST_CODE_PICK_IMAGE = 2
+
+    fun showImageChooser(activity: Activity) {
+        val galleryIntent = Intent(
+            Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        )
+        activity.startActivityForResult(galleryIntent, REQUEST_CODE_PICK_IMAGE)
+    }
 }
