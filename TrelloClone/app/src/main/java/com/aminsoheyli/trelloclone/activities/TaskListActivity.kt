11d30bdf33576531c0483bf87068a9889dd961ac
Amin Soheyli <aminsoheyli98@gmail.com>
2022-08-06 19:36:35 +0430

    Show task list recycler view

--- TrelloClone/app/src/main/java/com/aminsoheyli/trelloclone/activities/TaskListActivity.kt
+++ TrelloClone/app/src/main/java/com/aminsoheyli/trelloclone/activities/TaskListActivity.kt
@@ -1,11 +1,14 @@
 package com.aminsoheyli.trelloclone.activities
 
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.aminsoheyli.trelloclone.R
 import com.aminsoheyli.trelloclone.databinding.ActivityTaskListBinding
 import com.aminsoheyli.trelloclone.firebase.Firestore
 import com.aminsoheyli.trelloclone.models.Board
+import com.aminsoheyli.trelloclone.models.Task
 import com.aminsoheyli.trelloclone.utils.Constants
+import com.projemanag.adapters.TaskListItemsAdapter
 
 class TaskListActivity : BaseActivity() {
     private lateinit var boardDetails: Board
@@ -45,5 +48,12 @@ class TaskListActivity : BaseActivity() {
         boardDetails = board
         hideProgressDialog()
         setupActionBar()
+        val addTaskList = Task(resources.getString(R.string.add_list))
+        boardDetails.taskList.add(addTaskList)
+        binding.rvTaskList.layoutManager =
+            LinearLayoutManager(this@TaskListActivity, LinearLayoutManager.HORIZONTAL, false)
+        binding.rvTaskList.setHasFixedSize(true)
+        val adapter = TaskListItemsAdapter(this@TaskListActivity, boardDetails.taskList)
+        binding.rvTaskList.adapter = adapter
     }
 }
