ed1f3af99920285f307333a855d13ab991210806
Amin Soheyli <aminsoheyli98@gmail.com>
2022-08-13 03:16:13 +0430

    Add Card item adapter

---
+++ TrelloClone/app/src/main/java/com/aminsoheyli/trelloclone/adapters/CardItemsAdapter.kt
@@ -0,0 +1,37 @@
+package com.aminsoheyli.trelloclone.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.aminsoheyli.trelloclone.databinding.ItemCardBinding
+import com.aminsoheyli.trelloclone.models.Card
+
+open class CardItemsAdapter(
+    private val context: Context,
+    private var list: ArrayList<Card>
+) : RecyclerView.Adapter<CardItemsAdapter.ViewHolder>() {
+    private var onClickListener: OnClickListener? = null
+
+    class ViewHolder(val binding: ItemCardBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemCardBinding.inflate(LayoutInflater.from(context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val model = list[position]
+        holder.binding.tvCardName.text = model.name
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    fun setOnClickListener(onClickListener: OnClickListener) {
+        this.onClickListener = onClickListener
+    }
+
+    interface OnClickListener {
+        fun onClick(position: Int, card: Card)
+    }
+}
