e71821fb5de5e7641407165dd78059df47d2a160
Amin Soheyli <aminsoheyli98@gmail.com>
2022-08-17 17:06:27 +0430

    Add TopHeader composeable function

--- JetTip/app/src/main/java/com/aminsoheyli/jettip/MainActivity.kt
+++ JetTip/app/src/main/java/com/aminsoheyli/jettip/MainActivity.kt
@@ -3,41 +3,77 @@ package com.aminsoheyli.jettip
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.aminsoheyli.jettip.ui.theme.JetTipTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetTipTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            App {
+                TopHeader(134.4330)
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TopHeader(totalPerPerson: Double = 0.0) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+//            .clip(shape = CircleShape.copy(all = CornerSize(12.dp))),
+            .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
+        color = Color(0xFFE9D7F7)
+    ) {
+        Column(
+            modifier = Modifier.padding(vertical = 12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val totalPerPersonFormatted = "%.2f".format(totalPerPerson)
+            Text("Total Per Person ", style = MaterialTheme.typography.subtitle1)
+            Text(
+                "$$totalPerPersonFormatted", style = MaterialTheme.typography.h4,
+                fontWeight = FontWeight.ExtraBold
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun App(content: @Composable () -> Unit) {
+    JetTipTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            content()
+        }
+    }
+}
+
+//@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetTipTheme {
-        Greeting("Android")
+        App {
+            Text(text = "Hello Again")
+        }
     }
 }
