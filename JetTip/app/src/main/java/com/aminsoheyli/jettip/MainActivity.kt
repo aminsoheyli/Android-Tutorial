96bae2650b4c11f05d9cac06610a6ece9928d55a
Amin Soheyli <aminsoheyli98@gmail.com>
2022-08-17 18:33:38 +0430

    Add MainContent composeable function

--- JetTip/app/src/main/java/com/aminsoheyli/jettip/MainActivity.kt
+++ JetTip/app/src/main/java/com/aminsoheyli/jettip/MainActivity.kt
@@ -3,20 +3,27 @@ package com.aminsoheyli.jettip
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.aminsoheyli.jettip.components.InputField
 import com.aminsoheyli.jettip.ui.theme.JetTipTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,7 +37,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview
 @Composable
 fun TopHeader(totalPerPerson: Double = 0.0) {
     Surface(
@@ -56,6 +62,39 @@ fun TopHeader(totalPerPerson: Double = 0.0) {
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
+@Preview
+@Composable
+fun MainContent() {
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+    val keyboardController = LocalSoftwareKeyboardController.current
+    Surface(
+        modifier = Modifier
+            .padding(2.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 1.dp, color = Color.LightGray)
+    ) {
+        Column {
+            InputField(
+                valueState = totalBillState,
+                labelId = "Enter Bill",
+                enabled = true,
+                isSingleLine = true,
+                onAction = KeyboardActions {
+                    if (!validState) return@KeyboardActions
+                    keyboardController?.hide()
+                }
+            )
+        }
+    }
+}
+
 @Composable
 fun App(content: @Composable () -> Unit) {
     JetTipTheme {
@@ -73,7 +112,7 @@ fun App(content: @Composable () -> Unit) {
 fun DefaultPreview() {
     JetTipTheme {
         App {
-            Text(text = "Hello Again")
+            TopHeader(134.3350)
         }
     }
 }
