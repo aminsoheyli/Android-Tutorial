2aca62bf97142731dec2f76fdf6960f7b82e9a87
Amin Soheyli <aminsoheyli98@gmail.com>
2022-09-03 22:17:03 +0430

    Add a new TextView and use view-binding to change text

--- Databinding/app/src/main/java/com/aminsoheyli/databinding/MainActivity.kt
+++ Databinding/app/src/main/java/com/aminsoheyli/databinding/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.aminsoheyli.databinding
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import com.aminsoheyli.databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // View-Binding initialization
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUi()
+    }
+
+    private fun initUi() {
+        // View-Binding use-case
+        binding.editTextName.addTextChangedListener { watcher ->
+            binding.textViewBindingMessage.text = watcher.toString()
+        }
     }
 }
